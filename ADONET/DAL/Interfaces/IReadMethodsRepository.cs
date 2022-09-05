876b86b8e557c088528261465cc35864c1bca214
aliakseisatsura <asatsura@pfdrive.com>
2022-09-06 00:22:13 +0400

    add write, read and control methods

---
+++ ADONET/DAL/Interfaces/IReadMethodsRepository.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IReadMethodsRepository
+    {
+        Task<List<GoodModel>> GetAllGoodsAsync(SqlConnection connection);
+        Task<GoodModel> GetGoodByIdAsync(SqlConnection connection, int Id);
+    }
+}
---
+++ ADONET/DAL/Repository/WriteMethods.cs
@@ -0,0 +1,81 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DAL.Repository
+{
+    public class WriteMethods : IWriteMethodsRepository
+    {
+        public async Task AddListOfGoods(SqlConnection connection, List<GoodModel> goods)
+        {
+            SqlCommand cmd = new();
+            string commandText = "INSERT INTO Goods (GoodName, GoodType, Quantity, Price) VALUES";
+
+            for (int i = 0; i < goods.Count; i++)
+            {
+                if (!(i == goods.Count - 1))
+                {
+                    commandText += WriteComponentsToAddDataComand(goods[i]) + $", ";
+                }
+                else
+                {
+                    commandText += WriteComponentsToAddDataComand(goods[i]);
+                }
+            }
+
+            cmd.CommandText = commandText;
+            cmd.Connection = connection;
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+
+
+        public async Task DeleteData(SqlConnection connection, int Id)
+        {
+            SqlCommand cmd = new();
+            string commandText = $"DELETE FROM Goods WHERE Id='{Id}'";
+            cmd.CommandText = commandText;
+            cmd.Connection = connection;
+
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task UpdateData(SqlConnection connection, int Id, GoodModel good)
+        {
+            SqlCommand cmd = new();
+            string commandText = $"UPDATE Goods SET {WriteComponentsToUpdateDataComand(good)} WHERE Id='{Id}'";
+            cmd.CommandText = commandText;
+            cmd.Connection = connection;
+
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task AddGood(SqlConnection connection, GoodModel good)
+        {
+            SqlCommand cmd = new();
+            string commandText = $"INSERT INTO Goods (GoodName, GoodType, Quantity, Price) " +
+                $"VALUES('{good.GoodName}', '{good.GoodType}', '{good.Quantity}', '{good.Price}')";
+            cmd.CommandText = commandText;
+            cmd.Connection = connection;
+
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        private static string WriteComponentsToAddDataComand(GoodModel good)
+        {
+            string comand = $"('{good.GoodName}', '{good.GoodType}', '{good.Quantity}', '{good.Price}')";
+
+            return comand;
+        }
+
+        private static string WriteComponentsToUpdateDataComand(GoodModel good)
+        {
+            string comand = $"GoodName='{good.GoodName}'," +
+                $" GoodType='{good.GoodType}', " +
+                $"Quantity='{good.Quantity}'," +
+                $"Price='{good.Price}'";
+
+            return comand;
+        }
+    }
+}
